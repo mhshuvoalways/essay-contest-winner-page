738d91db6879229b94af1c44105a5d8f14fe0656
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2021-09-14 11:09:00 +0600

    dotenv add

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const importantDetail = require('./controllers/importantDetail');
 const photoGallery = require('./controllers/photoGallery');
 const magazine = require('./controllers/magazine');
 const mongoose = require('mongoose');
-const cloudinary = require('cloudinary').v2
-const port = 5000;
+const cloudinary = require('cloudinary').v2;
+const PORT = 5000 || process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,8 +26,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-mongoose.connect(process.env.MONGOURL);
-
-app.listen(port, () => {
-  console.log(`App listening at ${port}`)
-});
+mongoose.connect(`${process.env.MONGOURL}`)
+.then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening at ${PORT}`)
+  });
+})
+.catch((err) => {
+  console.log(err);
+})
