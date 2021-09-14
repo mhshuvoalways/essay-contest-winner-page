452dc213538f99d41609416697d677e5782bd72f
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2021-09-14 11:21:31 +0600

    port change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const photoGallery = require('./controllers/photoGallery');
 const magazine = require('./controllers/magazine');
 const mongoose = require('mongoose');
 const cloudinary = require('cloudinary').v2;
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
